22a9aed94a3cfb1c4477ac6cbafc3a8c403d7e7e
Zoltan Puchovsky <zoltan.puchovsky@gmail.com>
2019-07-13 21:26:31 +0200

    Initial Commit

---
+++ 531New/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  531New
+//
+//  Created by Zoltan Puchovsky on 13/07/2019.
+//  Copyright © 2019 Zoltan Puchovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
