3ec777566ed51598c13bd7cf4006bbcc239016b5
Zoltan Puchovsky <zoltan.puchovsky@gmail.com>
2019-07-13 23:32:38 +0200

    Picker View Added

--- 531New/ViewController.swift
+++ 531New/ViewController.swift
@@ -8,12 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData: [String] = [String] ()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Connect data:
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        // Input the data into the array
+        pickerData = ["Squat", "Overhead Press", "Deadlift", "Bench Press"]
     }
+    
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    
+
 
 
 }
